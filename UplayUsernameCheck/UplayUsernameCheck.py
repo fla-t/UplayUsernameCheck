2b1eca3ecceeacdf57b668775b6b7e7486095f2b
f-lat <61201330+f-lat@users.noreply.github.com>
2020-06-21 19:46:10 +0500

    Update UplayUsernameCheck.py

--- UplayUsernameCheck/UplayUsernameCheck.py
+++ UplayUsernameCheck/UplayUsernameCheck.py
@@ -1,2 +1,40 @@
-from selenium import webdriver
-from selenium
+import asyncio
+import itertools as it
+import r6sapi as api
+
+async def run():
+    auth = api.Auth("email", "password")
+
+    mainstring = "abcdefghijklmnopqrstuvwxyz"
+    secondstring = "abcdefghijklmnopqrstuvwxyz0123456789.-_"
+
+    for i in mainstring:
+      for j in secondstring:
+        for k in secondstring:
+          string = i + j + k
+          checkxbox = 0
+          checkpc = 0
+          checkpsn = 0
+          try:
+            await auth.get_player(string, api.Platforms.UPLAY)
+            checkpc = 1
+          except:
+            checkpc = 0
+
+          try:
+            await auth.get_player(string, api.Platforms.XBOX)
+            checkxbox = 1
+          except:
+            checkxbox = 0
+
+          try:
+            await auth.get_player(string, api.Platforms.PLAYSTATION)
+            checkpsn = 1
+          except:
+            checkpsn = 0
+
+          finally:
+            if (checkxbox == 0 and checkpsn == 0 and checkxbox == 0):
+              print(string)
+    
+asyncio.get_event_loop().run_until_complete(run())
